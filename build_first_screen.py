452ea19ce0911ff06bb5749afd130303fde76273
Luca Cavazzini <lucava9526@gmail.com>
2023-03-14 16:53:26 +0100

    getting the log in screen and the build function

---
+++ build_first_screen.py
@@ -0,0 +1,15 @@
+def build_first_screen(log_in):  
+    global text_box
+    label = Label(text = "", width=3, height=3)
+    label1 = Label(text = "insert username")
+    label_space = Label(text = "", width=1, height=1)
+    label_space1 = Label(text = "", width=1, height=1)
+
+    text_box = Text(height=1,width=10)
+    button_log = Button(text = "log in", command = log_in_function)
+    label.pack()
+    label1.pack()
+    label_space.pack()
+    text_box.pack()
+    label_space1.pack()
+    button_log.pack()
