f8392c9240cbcaac82b31cd7f7e2110bfb7dc495
Luca Cavazzini <lucava9526@gmail.com>
2023-03-28 19:31:30 +0200

    getting to the requirement

--- sql_queries.py
+++ sql_queries.py
@@ -6,6 +6,8 @@ import PIL.Image
 from tkinter import *
 from tkinter import messagebox
 
+import sys , os
+
 
 path_to_db = r"C:\Users\cavazzinil\Dropbox\naval battle code + ideas\naval_battle\naval_battle.db"
 
@@ -91,24 +93,32 @@ def retrieve_image(name, current_window ):
     label_picture.grid(row=1, column=0)
     
     
-def SaveBattle(name, frame_field, text, option):
+def SaveBattle(name_battle, field, text, options):
     #should get the different ships we have placed
-    ship_1 = [i for i in frame_field if i["bg"]=="orange"]
-    ship_2 = [i for i in frame_field if i["bg"]=="blue"]
-    ship_3 = [i for i in frame_field if i["bg"]=="purple"]
-    ship_4 = [i for i in frame_field if i["bg"]=="pink"]
-    
-    checking_the_ship = {"ship_1": (bool(len(ship_1)==1)),  "ship_2": (bool(len(ship_2)==2)),
-                         "ship_3":(bool(len(ship_3)==3)), "ship_4":(bool(len(ship_4)==4))}
-    #i check for the trues in the dict 
-    cases = {key: value for key, value in checking_the_ship.items() if value == True}
-    cases_negative = {key: value for key, value in checking_the_ship.items() if value == False}
-    #the conditions are that the elements on the textbox are taken and the list of the negative is ==0
-    name_battle_and_opponent = list(text.get("1.0", "end"),option.get())
-    
-    if len(list(cases_negative.keys())) == 0 and len( name_battle_and_opponent)==2:
-        #here i save to the db
+    try:
+        ship_1 = [i for i in field.grid_slaves() if i["bg"]=="orange"]
+        ship_2 = [i for i in field.grid_slaves() if i["bg"]=="blue"]
+        ship_3 = [i for i in field.grid_slaves() if i["bg"]=="purple"]
+        ship_4 = [i for i in field.grid_slaves() if i["bg"]=="pink"]
+        
+        checking_the_ship = {"ship_1": (bool(len(ship_1)==1)),  "ship_2": (bool(len(ship_2)==2)),
+                            "ship_3":(bool(len(ship_3)==3)), "ship_4":(bool(len(ship_4)==4))}
+        #i check for the trues in the dict 
+        cases = {key: value for key, value in checking_the_ship.items() if value == True}
+        cases_negative = {key: value for key, value in checking_the_ship.items() if value == False}
+        #the conditions are that the elements on the textbox are taken and the list of the negative is ==0
+        name_battle_and_opponent = list(text.get("1.0", "end")) + list(options.get())
+        
+        if len(list(cases_negative.keys())) == 0 and len( name_battle_and_opponent)==2:
+            #here i save to the db 
+            
+            pass
+        else:
+            print("reqiorement not satisfied")
         pass
-    else:
-        print("reqiorement not satisfied")
-    pass
+    except Exception as e :
+                    
+                    exc_type, exc_obj, exc_tb = sys.exc_info()
+                    fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+                    print(e, exc_type, fname, exc_tb.tb_lineno)
+                    messagebox.showinfo(message=str(e)+ "/n" + str(exc_type)+ "/n" + str(fname)+ "/n" + str(exc_tb.tb_lineno)+ "/n")
